b6df07c8708455a0bc092694e06f1c1135c36fbb
Rob Meijer <email@robmeijer.co.uk>
2017-12-10 18:47:34 +0000

    Migrate site to Symfony 4.0

---
+++ src/Migrations/Version20171210125634.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171210125634 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TABLE skill (id INTEGER NOT NULL, type VARCHAR(255) NOT NULL, keywords CLOB NOT NULL --(DC2Type:array)
+        , PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('DROP TABLE skill');
+    }
+}
